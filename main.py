5d602f8821e45484a32fca5db551439233c67d15
Jakub3Tp <154210380+Jakub3Tp@users.noreply.github.com>
2024-12-18 19:31:03 +0100

    18.12.2024

--- main.py
+++ main.py
@@ -23,9 +23,48 @@ class Cinema():
                 print(f"Klient {imie} {nazwisko} został dodany do kolejki")
 
             elif wybor == "2":
-                client = self.kolejka.queue(client)
-                if seat:
-                    print(f"Klient {client['imie']} {client['nazwisko']} został obsłużony")
+                klient = self.kolejka.obsluz_klienta()
+                if klient:
+                    miejsce = self.kino.przydziel_miejsce(klient)
+                    if miejsce:
+                        print(f"Klient {klient['imie']} {klient['nazwisko']} został obsłużony.")
+                        print(f"Przydzielono miejsce: rząd {miejsce[0] + 1}, miejsce {miejsce[1] + 1}.")
+                    else:
+                        print("Brak wolnych miejsc.")
+                else:
+                    print("Kolejka jest pusta.")
+
+            elif wybor == "3":
+                kolejka = self.kolejka.wyswietl_kolejke()
+                if kolejka:
+                    print("Aktualna kolejka:")
+                    for klient in kolejka:
+                        print(f"{klient['imie']} {klient['nazwisko']}")
+                else:
+                    print("Kolejka jest pusta.")
+
+            elif wybor == "4":
+                print("Aktualny stan sali kinowej:")
+                self.kino.wyswietl_sale()
+
+            elif wybor == "5":
+                historia = self.kino.wyswietl_historie()
+                if historia:
+                    print("Historia obsłużonych klientów:")
+                    for wpis in historia:
+                        klient = wpis["klient"]
+                        miejsce = wpis["miejsce"]
+                        print(f"{klient['imie']} {klient['nazwisko']} - rząd {miejsce[0] + 1}, miejsce {miejsce[1] + 1}")
+                else:
+                    print("Brak obsłużonych klientów.")
+
+            elif wybor == "6":
+                print("Zakończenie programu.")
+                break
+
+            else:
+                print("Nieprawidłowa opcja. Spróbuj ponownie.")
+
 
 if __name__ == '__main__':
     cinema = Cinema()
